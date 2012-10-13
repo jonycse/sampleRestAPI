1c4ed8b26cb7cf813e144b02d932c3ed7a11f2f8
Abu Zahed Jony <jony.cse@hotmail.com>
2012-10-13 17:31:06 +0600

    from validation added

--- api/bookapiextend/admin.py
+++ api/bookapiextend/admin.py
@@ -2,5 +2,30 @@ from django.contrib import admin
 
 from api.bookapiextend.models import Book, BookCategory
 
-admin.site.register(Book)
+from django import forms
+from django.forms.util import ErrorList
+
+import logging
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+
+    def clean(self):
+        try:
+            book_name = self.cleaned_data['name']
+            # form validation, error list required
+            if len(book_name)<5:
+                self._errors['name']=ErrorList(["Book name should have at least 5 characters"])
+        except:
+            pass
+        return self.cleaned_data
+
+
+class BookAdmin(admin.ModelAdmin):
+    form = BookForm
+
+
 admin.site.register(BookCategory)
+admin.site.register(Book, BookAdmin)
+
